547dcdebcb1135b1efe704ba97994ffa24c23aae
P4 <p4bl0eduardo2011@gmail.com>
2026-08-15 17:43:24 -0300

    Arvore de Natal: Meu pai que negocio complicado que eu inventei, vou deixar simples denovo e vou continuar.

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+	"strings"
+)
 
 var nome string = "Pablo"
 var idade int = 15
@@ -15,14 +19,45 @@ const filosofia string = "Por que algo em vez de nada?"
 func main() {
 	familia = "amor"
 
-	fmt.Println("Olá, Go")
-	soma(99, 15)
+	//fmt.Println("Olá, Go")
+	//soma(99, 15)
 
-	var inputIdade int
-	fmt.Print("\nDigite sua idade: ")
-	fmt.Scanln(&inputIdade)
+	//var inputIdade int
+	//fmt.Print("\nDigite sua idade: ")
+	//fmt.Scanln(&inputIdade)
 
-	faseDaVida(inputIdade)
+	//faseDaVida(inputIdade)
+
+	var espaços string = ""
+	var tamanhoDaBase int = 10
+
+	for i := 0; i < tamanhoDaBase; i++ {
+		acumulado := "# "
+
+		for n := tamanhoDaBase - 1; n >= i; n-- {
+			espaços += " "
+		}
+		if i == 0 {
+			resultado := espaços + "*"
+			fmt.Println(resultado)
+			espaços = strings.TrimSpace(espaços)
+		} else {
+
+			for j := 0; j < i; j++ {
+				if rand.IntN(4) == 0 {
+					acumulado += "O "
+				} else {
+					acumulado += "# "
+				}
+			}
+
+			resultado := espaços + acumulado
+			fmt.Println(resultado)
+			espaços = strings.TrimSpace(espaços)
+		}
+	}
+
+	fmt.Scanln()
 }
 
 func soma(a int, b int) {
